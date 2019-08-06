daa0135f36fc6f43193e64a5de39202574a1bc7b
Nathan Franklin <nathan.franklin@gmail.com>
2019-08-06 12:57:20 -0500

    Fix test

--- geoapi/tests/test_services_lidar.py
+++ geoapi/tests/test_services_lidar.py
@@ -8,4 +8,4 @@ def test_get_epsg_las_1pt2(lidar_las1pt2_file_path_fixture):
 
 def test_get_epsg_las_1pt4(lidar_las1pt4_file_path_fixture):
     epsg = LidarService.getEPSG(lidar_las1pt4_file_path_fixture)
-    assert epsg == 26913s
+    assert epsg == 26913
