e27a7cbbae09840b054a992606507fb60671b8a7
Nathan Franklin <nathan.franklin@gmail.com>
2020-01-08 14:07:40 -0600

    Add test for bounding box filtering

--- geoapi/tests/api_tests/test_projects_routes.py
+++ geoapi/tests/api_tests/test_projects_routes.py
@@ -93,10 +93,21 @@ def test_get_project_features_single_feature(test_client, dbsession, projects_fi
     assert len(data['features']) != 0
 
 
-def test_get_project_features_filter(test_client, dbsession, projects_fixture, feature_fixture, image_feature_fixture):
+def test_get_project_features_filter_with_assettype(test_client, dbsession, projects_fixture, feature_fixture, image_feature_fixture):
     u1 = dbsession.query(User).get(1)
     resp = test_client.get('/projects/1/features/',
                            query_string={'assetType': 'image'},
+                           headers={'x-jwt-assertion-test': u1.jwt})
+    data = resp.get_json()
+    assert resp.status_code == 200
+    assert len(data['features']) == 1
+
+
+def test_get_project_features_filter_with_bounding_box(test_client, dbsession, projects_fixture, feature_fixture, image_feature_fixture):
+    bbox = [-80.9, 32.61, -80, 32.62]
+    u1 = dbsession.query(User).get(1)
+    resp = test_client.get('/projects/1/features/',
+                           query_string='bbox={}'.format(','.join(map(str, bbox))),
                            headers={'x-jwt-assertion-test': u1.jwt})
     data = resp.get_json()
     assert resp.status_code == 200
