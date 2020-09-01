d14929342d0bef0156b869ebeaf6c9591cbfb3ac
Nathan Franklin <nathan.franklin@gmail.com>
2020-09-01 10:48:57 -0500

    Add unit tests for rolling back

--- geoapi/tests/external_data_tests/test_external_data.py
+++ geoapi/tests/external_data_tests/test_external_data.py
@@ -11,39 +11,55 @@ from geoapi.exceptions import InvalidCoordinateReferenceSystem
 from geoapi.services.point_cloud import PointCloudService
 
 
-@patch("geoapi.tasks.external_data.AgaveUtils")
-def test_external_data_good_files(MockAgaveUtils, userdata, projects_fixture, geojson_file_fixture):
-    filesListing = [
-        AgaveFileListing({
-            "system": "testSystem",
-            "path": "/testPath",
-            "type": "dir",
-            "length": 4,
-            "_links": "links",
-            "mimeType": "folder",
-            "lastModified": "2020-08-31T12:00:00Z"
-        }),
-        AgaveFileListing({
-            "system": "testSystem",
-            "type": "file",
-            "length": 4096,
-            "path": "/testPath/file.json",
-            "_links": "links",
-            "mimeType": "application/json",
-            "lastModified": "2020-08-31T12:00:00Z"
-        })
-    ]
+@pytest.fixture(scope="function")
+def rollback_side_effect():
+    with patch('geoapi.db.db_session.rollback', side_effect=db_session.rollback) as rollback:
+        yield rollback
+
+
+@pytest.fixture(scope="function")
+def db_session_commit_throws_exception():
+    with patch('geoapi.db.db_session.commit', side_effect=Exception) as commit:
+        yield commit
+
+@pytest.fixture(scope="function")
+def agave_utils_with_geojson_file(geojson_file_fixture):
+    with patch('geoapi.tasks.external_data.AgaveUtils') as MockAgaveUtils:
+        filesListing = [
+            AgaveFileListing({
+                "system": "testSystem",
+                "path": "/testPath",
+                "type": "dir",
+                "length": 4,
+                "_links": "links",
+                "mimeType": "folder",
+                "lastModified": "2020-08-31T12:00:00Z"
+            }),
+            AgaveFileListing({
+                "system": "testSystem",
+                "type": "file",
+                "length": 4096,
+                "path": "/testPath/file.json",
+                "_links": "links",
+                "mimeType": "application/json",
+                "lastModified": "2020-08-31T12:00:00Z"
+            })
+        ]
+        MockAgaveUtils().listing.return_value = filesListing
+        MockAgaveUtils().getFile.return_value = geojson_file_fixture
+        yield MockAgaveUtils()
+
+
+def test_external_data_good_files(userdata, projects_fixture, agave_utils_with_geojson_file):
     u1 = db_session.query(User).filter(User.username == "test1").first()
-    proj = db_session.query(Project).get(1)
-    MockAgaveUtils().listing.return_value = filesListing
-    MockAgaveUtils().getFile.return_value = geojson_file_fixture
-    import_from_agave(u1.id, "testSystem", "/testPath", proj.id)
+
+    import_from_agave(u1.id, "testSystem", "/testPath", projects_fixture.id)
     features = db_session.query(Feature).all()
     # the test geojson has 3 features in it
     assert len(features) == 3
     # This should only have been called once, since there is only
     # one FILE in the listing
-    assert MockAgaveUtils().getFile.called_once()
+    agave_utils_with_geojson_file.getFile.assert_called_once()
 
 
 @pytest.mark.worker
@@ -71,10 +87,10 @@ def test_import_point_clouds_from_agave(MockAgaveUtils,
 @patch("geoapi.tasks.external_data.check_point_cloud")
 @patch("geoapi.tasks.external_data.AgaveUtils")
 def test_import_point_clouds_from_agave_check_point_cloud_missing_crs(MockAgaveUtils,
-                                        check_mock,
-                                        projects_fixture,
-                                        point_cloud_fixture,
-                                        lidar_las1pt2_file_fixture):
+                                                                      check_mock,
+                                                                      projects_fixture,
+                                                                      point_cloud_fixture,
+                                                                      lidar_las1pt2_file_fixture):
     MockAgaveUtils().getFile.return_value = lidar_las1pt2_file_fixture
     check_mock.apply.side_effect = InvalidCoordinateReferenceSystem()
 
@@ -92,10 +108,10 @@ def test_import_point_clouds_from_agave_check_point_cloud_missing_crs(MockAgaveU
 @patch("geoapi.tasks.external_data.check_point_cloud")
 @patch("geoapi.tasks.external_data.AgaveUtils")
 def test_import_point_clouds_from_agave_check_point_cloud_unknown(MockAgaveUtils,
-                                        check_mock,
-                                        projects_fixture,
-                                        point_cloud_fixture,
-                                        lidar_las1pt2_file_fixture):
+                                                                  check_mock,
+                                                                  projects_fixture,
+                                                                  point_cloud_fixture,
+                                                                  lidar_las1pt2_file_fixture):
     MockAgaveUtils().getFile.return_value = lidar_las1pt2_file_fixture
     check_mock.apply.side_effect = Exception("dummy")
 
@@ -113,10 +129,10 @@ def test_import_point_clouds_from_agave_check_point_cloud_unknown(MockAgaveUtils
 @patch("geoapi.tasks.external_data.convert_to_potree")
 @patch("geoapi.tasks.external_data.AgaveUtils")
 def test_import_point_clouds_from_agave_conversion_error(MockAgaveUtils,
-                                        convert_mock,
-                                        projects_fixture,
-                                        point_cloud_fixture,
-                                        lidar_las1pt2_file_fixture):
+                                                         convert_mock,
+                                                         projects_fixture,
+                                                         point_cloud_fixture,
+                                                         lidar_las1pt2_file_fixture):
     MockAgaveUtils().getFile.return_value = lidar_las1pt2_file_fixture
     convert_mock.apply.side_effect = Exception("dummy")
 
@@ -128,3 +144,33 @@ def test_import_point_clouds_from_agave_conversion_error(MockAgaveUtils,
     assert point_cloud.task.status == "FAILED"
     assert point_cloud.task.description == ""
     assert len(os.listdir(get_asset_path(point_cloud_fixture.path, PointCloudService.ORIGINAL_FILES_DIR))) == 1
+
+
+@pytest.mark.worker
+@patch("geoapi.tasks.external_data.AgaveUtils")
+def test_import_point_clouds_failed_dbsession_rollback(MockAgaveUtils,
+                                                       projects_fixture,
+                                                       point_cloud_fixture,
+                                                       lidar_las1pt2_file_fixture,
+                                                       db_session_commit_throws_exception,
+                                                       rollback_side_effect):
+    MockAgaveUtils().getFile.return_value = lidar_las1pt2_file_fixture
+
+    u1 = db_session.query(User).get(1)
+    files = [{"system": "designsafe.storage.default", "path": "file1.las"}]
+
+    with pytest.raises(Exception):
+        import_point_clouds_from_agave(u1.id, files, point_cloud_fixture.id)
+
+    rollback_side_effect.assert_called_once()
+
+
+def test_import_from_agave_failed_dbsession_rollback(agave_utils_with_geojson_file,
+                                                     userdata,
+                                                     projects_fixture,
+                                                     db_session_commit_throws_exception,
+                                                     rollback_side_effect):
+    with pytest.raises(Exception):
+        import_from_agave(userdata.id, "testSystem", "/testPath", projects_fixture.id)
+
+    rollback_side_effect.assert_called()
