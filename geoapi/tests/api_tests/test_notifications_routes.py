ccb8bc87ffe1b71359f281b7a6607ec9b8cfe36c
Nathan Franklin <nathan.franklin@gmail.com>
2021-04-02 21:16:16 -0500

    Fix pep8

--- geoapi/tests/api_tests/test_notifications_routes.py
+++ geoapi/tests/api_tests/test_notifications_routes.py
@@ -1,5 +1,5 @@
 import pytest
-from geoapi.models import User, Notification
+from geoapi.models import User
 from geoapi.db import db_session
 from geoapi.services.notifications import NotificationsService
 
@@ -7,7 +7,6 @@ from geoapi.services.notifications import NotificationsService
 @pytest.fixture
 def notifications(userdata):
     u1 = db_session.query(User).filter(User.username == "test1").first()
-    u2 = db_session.query(User).filter(User.username == "test2").first()
     NotificationsService.create(u1, "error", "test error")
     NotificationsService.create(u1, "success", "test success")
 
@@ -16,25 +15,27 @@ def test_get_notifications(test_client, notifications):
     u1 = db_session.query(User).get(1)
 
     resp = test_client.get('/notifications/',
-                            headers={'x-jwt-assertion-test': u1.jwt})
+                           headers={'x-jwt-assertion-test': u1.jwt})
     data = resp.get_json()
     assert resp.status_code == 200
     assert len(data) == 2
+
 
 def test_filter_notifications(test_client, notifications):
     u1 = db_session.query(User).get(1)
 
     resp = test_client.get('/notifications/?startDate=2222-1-1T12:00:00+00:00',
-                            headers={'x-jwt-assertion-test': u1.jwt})
+                           headers={'x-jwt-assertion-test': u1.jwt})
     data = resp.get_json()
     assert resp.status_code == 200
     assert len(data) == 0
+
 
 def test_filter_notifications_positive(test_client, notifications):
     u1 = db_session.query(User).get(1)
 
     resp = test_client.get('/notifications/?startDate=1900-1-1T12:00:00+00:00',
-                            headers={'x-jwt-assertion-test': u1.jwt})
+                           headers={'x-jwt-assertion-test': u1.jwt})
     data = resp.get_json()
     assert resp.status_code == 200
     assert len(data) == 2
