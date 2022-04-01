a15ba21802b9967192982888e9af6b4d89599006
Nathan Franklin <nathan.franklin@gmail.com>
2022-04-01 09:41:52 -0500

    Add merge migration

---
+++ geoapi/migrations/versions/b014b0e37b0b_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: b014b0e37b0b
+Revises: 03c3d62d3b11, 63e1135553fe
+Create Date: 2022-04-01 03:04:29.358825
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b014b0e37b0b'
+down_revision = ('03c3d62d3b11', '63e1135553fe')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
