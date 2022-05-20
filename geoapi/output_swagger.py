2e3317f549b364afacca92f550a31ca414b10f2e
Nathan Franklin <nathan.franklin@gmail.com>
2022-05-20 16:21:23 -0500

    Fix some linting errors

--- geoapi/output_swagger.py
+++ geoapi/output_swagger.py
@@ -2,10 +2,12 @@ from geoapi.app import api, app
 from flask import json
 import argparse
 
+
 def write_openapi_definition(filename):
     with app.test_request_context():
         with open(filename, 'w') as f:
-           f.write(json.dumps(api.__schema__))
+            f.write(json.dumps(api.__schema__))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Output swagger file")
