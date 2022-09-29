5b0658a08e220e2e4076f8a836eaf0fe0adadd61
Nathan Franklin <nathan.franklin@gmail.com>
2022-09-29 10:25:52 -0500

    Hotfix/project deletion (#104)
    * User logger from geoapi.log
    
    * Add missing db_session.commit after project deletion
    
    * Add another delete test
    
    Note that this test doesn't catch the regression.  Not sure why the
    db_session.commit isn't needed in the unit test. I don't see it but its
    like the db_session is configured for auto-commit
    
    * Update geoapi/tests/api_tests/test_projects_service.py
    
    Co-authored-by: Ian Park <contact@duckonomy.com>
    
    Co-authored-by: Ian Park <contact@duckonomy.com>

--- geoapi/services/projects.py
+++ geoapi/services/projects.py
@@ -12,10 +12,8 @@ from geoapi.utils.agave import AgaveUtils, get_system_users
 from geoapi.utils.assets import get_project_asset_dir
 from geoapi.utils.users import is_anonymous
 from geoapi.tasks.external_data import import_from_agave
-from geoapi.log import logging
+from geoapi.log import logger
 from geoapi.exceptions import ApiException, ObservableProjectAlreadyExists
-
-logger = logging.getLogger(__name__)
 
 
 class ProjectsService:
@@ -274,6 +272,7 @@ class ProjectsService:
         :return:
         """
         db_session.query(Project).filter(Project.id == projectId).delete()
+        db_session.commit()
 
         assets_folder = get_project_asset_dir(projectId)
         try:
