bc3fcaac9dee4765065457d139074f91f247ab82
Nathan Franklin <nathan.franklin@gmail.com>
2023-01-23 12:59:44 -0600

    Add remove_project_assets to __init__.py

--- geoapi/tasks/__init__.py
+++ geoapi/tasks/__init__.py
@@ -1,3 +1,4 @@
 from geoapi.tasks.lidar import convert_to_potree
 from geoapi.tasks.external_data import import_file_from_agave, import_from_agave, refresh_observable_projects
 from geoapi.tasks.streetview import publish, from_tapis_to_streetview, process_streetview_sequences
+from geoapi.tasks.projects import remove_project_assets
