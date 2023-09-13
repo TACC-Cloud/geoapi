914a77995c1bf6de653251706321a8cad29e4d82
Nathan Franklin <nathan.franklin@gmail.com>
2023-09-13 14:22:37 -0500

    Add session creator for tasks

--- geoapi/db.py
+++ geoapi/db.py
@@ -3,7 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.pool import NullPool
 from geoapi.settings import settings
+from contextlib import contextmanager
 import os
+from geoapi.log import logger
 
 
 CONNECTION_STRING = 'postgresql://{}:{}@{}/{}'.format(
@@ -14,8 +16,9 @@ CONNECTION_STRING = 'postgresql://{}:{}@{}/{}'.format(
 )
 
 
-def create_engine_for_context():
+def create_engine_for_context(context=None):
     context = os.environ.get('APP_CONTEXT', 'flask')  # Default to 'flask' if not provided
+    logger.debug(f"Creating database engine for {context} context.")
     if context == "celery":
         # celery to disable pool (i.e. NullPool) due to https://jira.tacc.utexas.edu/browse/WG-141 and
         # https://jira.tacc.utexas.edu/browse/WG-131
@@ -32,8 +35,27 @@ def create_engine_for_context():
 
 engine = create_engine_for_context()
 
+# thread-local db session (which is used by flask requests)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 
 Base = declarative_base()
+
+
+@contextmanager
+def create_task_session():
+    """Create session
+
+    Session is used by celery tasks: it ensures they are removed and handles rollback in case of exceptions
+    """
+    Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    session = Session()
+    try:
+        yield session
+    except:
+        logger.exception("Error occurred. Performing rollback of current database transaction")
+        session.rollback()
+        raise
+    finally:
+        session.close()
