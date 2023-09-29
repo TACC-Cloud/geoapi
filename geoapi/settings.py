72366638f8105b46991982fb35b3e208e64bc1b0
Nathan Franklin <nathan.franklin@gmail.com>
2023-09-29 14:46:35 -0500

    task/WG-134 setup docker compose for geoapi services vm (#142)
    * Add initial/work-in-progress docker-compose
    
    * Add nginx conf
    
    * Move docker infrastructure to devops folder
    
    * Move remaining docker infrastructure to devops folder
    
    * Fix local development's docker-compose
    
    * Add additional improvements to docker-compose for geoapi-services vms
    
    * Add some variables
    
    * Use one env variable for mapillary
    
    * Fix dockerfiles
    
    * Add secret.env to gitignore
    
    * Update docker-compose test yaml
    
    * Fix installing of python packages on github actions workflow
    
    * Change to devops/ before running test on CI workflow
    
    * Simplify install/pytest step in CI workflow
    
    * Refactor location of dockerfiles and images used
    
    * Fix nginx conf for deployment
    
    * Change secrets location to /opt/portal/conf/secret.env
    
    * Use absolute path for secrets.env
    
    * Add makefile for geoapi-services
    
    * Refactor dockerfile
    
    * Correct path to poetry.lock and pyproject.toml
    
    * Fix paths in dockerfile
    
    * Rename Dockerfile.potree to Dockerfile.worker
    
    * Rename Dockerfile.potree to Dockerfile.worker again
    
    * Fix workflow
    
    * Fix workflow
    
    * Remove env file usage in Makefile
    
    Using docker-compose.yml where that is hardcoded
    
    * Pass in GEOAPI_TAG from env
    
    * Remove echo
    
    * Fix docker compose statement
    
    * update docker compose and Makefile
    
    * Fix MAKEFILE
    
    * Add todo
    
    * Fix MAKEFILE again
    
    * Add missing RABBITMQ_VHOST
    
    * Do not run docker up in background for testing
    
    * Fix gitignore and improve dockerignore
    
    * Bump rabbitmq version
    
    * Update todo
    
    * Fix tabbitmq password variable on example secrets file
    
    * Refactor how settings are set based on APP_ENV
    
    * Add hazmapper to docker-compose
    
    * Fix makefile
    
    * Expose rabbitmq port from geoapi-services
    
    * Improve make build command
    
    * Improve deploy command
    
    * Add a copy of poetry files
    
    * Fix unit test settings
    
    * Move to devops before running test
    
    * Removed unused steps in workflow

--- geoapi/settings.py
+++ geoapi/settings.py
@@ -4,22 +4,14 @@ import os
 class Config(object):
     DEBUG = False
     TESTING = False
-    DB_USERNAME = 'dev'
-    DB_NAME = 'dev'
-    DB_PASSWD = 'dev'
-    DB_HOST = 'postgres'
     ASSETS_BASE_DIR = os.environ.get('ASSETS_BASE_DIR', '/assets')
     JWT_PUB_KEY = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCUp/oV1vWc8/TkQSiAvTousMzOM4asB2iltr2QKozni5aVFu818MpOLZIr8LMnTzWllJvvaA5RAAdpbECb+48FjbBe0hseUdN5HpwvnH/DW8ZccGvk53I6Orq7hLCv1ZHtuOCokghz/ATrhyPq+QktMfXnRS4HrKGJTzxaCcU7OQIDAQAB'  # noqa: E501
-    RABBITMQ_USERNAME = 'dev'
-    RABBITMQ_PASSWD = 'dev'
-    RABBITMQ_VHOST = 'dev'
-    RABBITMQ_HOSTNAME = 'rabbitmq'
     RESTPLUS_MASK_SWAGGER = False
     TENANT = os.environ.get("TENANT")
     STREETVIEW_DIR = os.environ.get('STREETVIEW_DIR', '/assets/streetview')
 
 
-class ProductionConfig(Config):
+class DeployedConfig(Config):
     DEBUG = False
     DB_USERNAME = os.environ.get("DB_USERNAME")
     DB_NAME = os.environ.get("DB_NAME")
@@ -32,16 +24,22 @@ class ProductionConfig(Config):
     MAPILLARY_CLIENT_ID = os.environ.get('MAPILLARY_CLIENT_ID', 'MLY|5156692464392931|4f1118aa1b06f051a44217cb56bedf79')
 
 
-class DevelopmentConfig(Config):
+class LocalDevelopmentConfig(Config):
     DEBUG = True
     PROPAGATE_EXCEPTIONS = False
+    RABBITMQ_USERNAME = 'dev'
+    RABBITMQ_PASSWD = 'dev'
+    RABBITMQ_VHOST = 'dev'
+    RABBITMQ_HOSTNAME = 'rabbitmq'
+    DB_USERNAME = 'dev'
+    DB_NAME = 'dev'
+    DB_PASSWD = 'dev'
+    DB_HOST = 'postgres'
     MAPILLARY_CLIENT_ID = os.environ.get('MAPILLARY_CLIENT_ID', 'MLY|4866220476802272|cedfb10deac752ca3ddf83997cef60a4')
 
 
-class TestingConfig(Config):
-    DB_USERNAME = 'dev'
+class UnitTestingConfig(LocalDevelopmentConfig):
     DB_NAME = 'test'
-    DB_PASSWD = 'dev'
     DB_HOST = os.environ.get('DB_HOST', 'postgres')
     TESTING = True
     STREETVIEW_DIR = os.environ.get('STREETVIEW_DIR', '/tmp/streetview')
@@ -51,9 +49,9 @@ class TestingConfig(Config):
 
 
 APP_ENV = os.environ.get('APP_ENV', '').lower()
-if APP_ENV == 'production':
-    settings = ProductionConfig
+if APP_ENV == 'production' or APP_ENV == 'staging' or APP_ENV == 'dev':
+    settings = DeployedConfig
 elif APP_ENV == 'testing':
-    settings = TestingConfig
+    settings = UnitTestingConfig
 else:
-    settings = DevelopmentConfig
+    settings = LocalDevelopmentConfig
