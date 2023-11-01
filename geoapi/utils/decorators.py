43c0f02e77cce99517c7976ef7695e724dbf3b56
Nathan Franklin <nathan.franklin@gmail.com>
2023-11-01 13:06:20 -0500

    Adding comment

--- geoapi/utils/decorators.py
+++ geoapi/utils/decorators.py
@@ -33,6 +33,7 @@ def jwt_decoder(fn):
         try:
             jwt_header_name, token, tenant = jwt_utils.jwt_tenant(request.headers)
         except ValueError:
+            # if not JWT information is provided in header, then this is a guest user
             guest_uuid = request.headers.get('X-Guest-UUID')
             user = AnonymousUser(guest_unique_id=guest_uuid)
         if user is None:
