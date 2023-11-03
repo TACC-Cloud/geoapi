d334c14a82b581adee18aa7d7ece623ff75e431b
Nathan Franklin <nathan.franklin@gmail.com>
2023-11-03 16:03:24 -0500

    task/WG-139: add uuid for guest users (#155)
    * Derive guest name from header's X-Guest-UUID
    
    * Adding comment

--- geoapi/utils/users.py
+++ geoapi/utils/users.py
@@ -1,5 +1,18 @@
 class AnonymousUser:
-    username = "Guest"
+    """
+    Represents an anonymous user.
+
+    Attributes:
+        username (str): A unique username for the anonymous user, prefixed by 'Guest_'.
+                       If no `guest_unique_id` is provided, username defaults to 'Guest_Unknown'.
+
+    Args:
+        guest_unique_id (Optional[str]): A unique identifier for the guest user.
+    """
+
+    def __init__(self, guest_unique_id=None):
+        guest_uid = guest_unique_id if guest_unique_id else "Unknown"
+        self.username = f"Guest_{guest_uid}"
 
 
 def is_anonymous(user):
