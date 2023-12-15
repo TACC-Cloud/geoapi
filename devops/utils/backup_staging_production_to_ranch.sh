c04f57812875b667c1f5385d5dace6ba9dac39b5
Nathan Franklin <nathan.franklin@gmail.com>
2023-12-14 19:46:13 -0600

    Turn off strick host key checking

--- devops/utils/backup_staging_production_to_ranch.sh
+++ devops/utils/backup_staging_production_to_ranch.sh
@@ -2,7 +2,7 @@
 set -ex
 
 echo "Removing backups older than 6 weeks (i.e. 42 days) (STAGING)"
-ssh tg458981@ranch.tacc.utexas.edu 'find /stornext/ranch_01/ranch/projects/DesignSafe-Community/geoapi_assets_backup/staging/ -mtime +42 -type f -exec rm {} +'
+ssh -o StrictHostKeyChecking=no tg458981@ranch.tacc.utexas.edu 'find /stornext/ranch_01/ranch/projects/DesignSafe-Community/geoapi_assets_backup/staging/ -mtime +42 -type f -exec rm {} +'
 
 echo "Backing up staging"
 ssh -o StrictHostKeyChecking=no portal@prod.geoapi-services.tacc.utexas.edu "
@@ -14,13 +14,13 @@ ssh tg458981@ranch.tacc.utexas.edu 'split -b 300G - /stornext/ranch_01/ranch/pro
 echo "Finished with STAGING and beginning PRODUCTION"
 
 echo "Removing backups older than 6 weeks (i.e. 42 days) (PRODUCTION)"
-ssh tg458981@ranch.tacc.utexas.edu 'find /stornext/ranch_01/ranch/projects/DesignSafe-Community/geoapi_assets_backup/production/ -mtime +42 -type f -exec rm {} +'
+ssh -o StrictHostKeyChecking=no tg458981@ranch.tacc.utexas.edu 'find /stornext/ranch_01/ranch/projects/DesignSafe-Community/geoapi_assets_backup/production/ -mtime +42 -type f -exec rm {} +'
 
 # testing
 exit 1
 
 echo "Backing up production"
-ssh -o StrictHostKeyChecking=no ortal@prod.geoapi-services.tacc.utexas.edu "
+ssh -o StrictHostKeyChecking=no portal@prod.geoapi-services.tacc.utexas.edu "
 set -ex
 # skipping /assets/streetview as those are temp files
 tar --exclude=/assets/streetview -c -f - /assets/ | \
