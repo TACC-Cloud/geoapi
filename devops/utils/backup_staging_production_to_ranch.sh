8563e9545452196239f646797a195522472efdf5
Nathan Franklin <nathan.franklin@gmail.com>
2023-12-14 20:03:08 -0600

    Fix command substitution

--- devops/utils/backup_staging_production_to_ranch.sh
+++ devops/utils/backup_staging_production_to_ranch.sh
@@ -9,9 +9,13 @@ ssh -o StrictHostKeyChecking=no portal@staging.geoapi-services.tacc.utexas.edu "
 set -ex
 # skipping /assets/streetview as those are temp files
 tar --exclude=/assets/streetview -c -f - /assets/ | \
-ssh -o StrictHostKeyChecking=no tg458981@ranch.tacc.utexas.edu 'split -b 300G - /stornext/ranch_01/ranch/projects/DesignSafe-Community/geoapi_assets_backup/staging/staging_assets\$(date +%Y-%m-%d).tar.'
+ssh -o StrictHostKeyChecking=no tg458981@ranch.tacc.utexas.edu 'split -b 300G - /stornext/ranch_01/ranch/projects/DesignSafe-Community/geoapi_assets_backup/staging/staging_assets\\$(date +%Y-%m-%d).tar.'
 "
+
 echo "Finished with STAGING and beginning PRODUCTION"
+
+echo "--------------------------------------------------"
+echo "--------------------------------------------------"
 
 echo "Removing backups older than 6 weeks (i.e. 42 days) (PRODUCTION)"
 ssh -o StrictHostKeyChecking=no tg458981@ranch.tacc.utexas.edu 'find /stornext/ranch_01/ranch/projects/DesignSafe-Community/geoapi_assets_backup/production/ -mtime +42 -type f -exec rm {} +'
@@ -21,5 +25,5 @@ ssh -o StrictHostKeyChecking=no portal@prod.geoapi-services.tacc.utexas.edu "
 set -ex
 # skipping /assets/streetview as those are temp files
 tar --exclude=/assets/streetview -c -f - /assets/ | \
-ssh -o StrictHostKeyChecking=no tg458981@ranch.tacc.utexas.edu 'split -b 300G - /stornext/ranch_01/ranch/projects/DesignSafe-Community/geoapi_assets_backup/production/production_assets\$(date +%Y-%m-%d).tar.'
+ssh -o StrictHostKeyChecking=no tg458981@ranch.tacc.utexas.edu 'split -b 300G - /stornext/ranch_01/ranch/projects/DesignSafe-Community/geoapi_assets_backup/production/production_assets\\$(date +%Y-%m-%d).tar.'
 "
