2c451e43075d95e73d27afd00e09ae583dc76dbf
Nathan Franklin <nathan.franklin@gmail.com>
2023-12-22 11:19:48 -0600

    task/WG-95: update asset backup (#167)
    * Add backup script for assets
    
    * Make executable
    
    * Turn off strick host key checking
    
    * Disable strict host checking
    
    * Fix command substitution
    
    * Exclude bug and lost&found

---
+++ devops/utils/backup_staging_production_to_ranch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -ex
+
+echo "Removing backups older than 6 weeks (i.e. 42 days) (STAGING)"
+ssh -o StrictHostKeyChecking=no tg458981@ranch.tacc.utexas.edu 'find /stornext/ranch_01/ranch/projects/DesignSafe-Community/geoapi_assets_backup/staging/ -mtime +42 -type f -exec rm {} +'
+
+echo "Backing up staging"
+ssh -o StrictHostKeyChecking=no portal@staging.geoapi-services.tacc.utexas.edu "
+set -ex
+# skipping /assets/streetview as those are temp files
+tar --exclude=/assets/streetview --exclude=/assets/lost+found --exclude=/assets/bug/ -c -f - /assets/ | \
+ssh -o StrictHostKeyChecking=no tg458981@ranch.tacc.utexas.edu 'split -b 300G - /stornext/ranch_01/ranch/projects/DesignSafe-Community/geoapi_assets_backup/staging/staging_assets\\$(date +%Y-%m-%d).tar.'
+"
+
+echo "Finished with STAGING and beginning PRODUCTION"
+
+echo "--------------------------------------------------"
+echo "--------------------------------------------------"
+
+echo "Removing backups older than 6 weeks (i.e. 42 days) (PRODUCTION)"
+ssh -o StrictHostKeyChecking=no tg458981@ranch.tacc.utexas.edu 'find /stornext/ranch_01/ranch/projects/DesignSafe-Community/geoapi_assets_backup/production/ -mtime +42 -type f -exec rm {} +'
+
+echo "Backing up production"
+ssh -o StrictHostKeyChecking=no portal@prod.geoapi-services.tacc.utexas.edu "
+set -ex
+# skipping /assets/streetview as those are temp files
+tar --exclude=/assets/streetview  --exclude=/assets/lost+found --exclude=/assets/bug/ -c -f - /assets/ | \
+ssh -o StrictHostKeyChecking=no tg458981@ranch.tacc.utexas.edu 'split -b 300G - /stornext/ranch_01/ranch/projects/DesignSafe-Community/geoapi_assets_backup/production/production_assets\\$(date +%Y-%m-%d).tar.'
+"
