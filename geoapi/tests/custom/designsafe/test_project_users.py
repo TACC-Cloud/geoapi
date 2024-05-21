7a2d7c5a98ab23601bb55eefc4075ae0ea8af2bb
Nathan Franklin <nathan.franklin@gmail.com>
2024-05-21 10:46:52 -0500

    Use DesignSafe api for getting users
    Code was previusly updated but we were using a fixture. So fixture is removed and we are now using
    DesignSafe's /api/projects/v2 to get project information on users.

--- geoapi/tests/custom/designsafe/test_project_users.py
+++ geoapi/tests/custom/designsafe/test_project_users.py
@@ -1,8 +1,8 @@
 import os
 import pytest
-from unittest.mock import patch
 import json
 from geoapi.custom.designsafe.project_users import get_system_users
+from geoapi.settings import settings
 
 
 @pytest.fixture()
@@ -10,10 +10,6 @@ def project_response():
     home = os.path.dirname(__file__)
     with open(os.path.join(home, '../../fixtures/designsafe_api_project.json'), 'rb') as f:
         mock_data = json.loads(f.read())
-
-    # TODO_TAPISV3 https://tacc-main.atlassian.net/browse/WG-257 can be dropped later as we rely on requests_mock
-    # in tests
-    with patch('geoapi.custom.designsafe.project_users.get_project_data', return_value=mock_data["value"]):
         yield mock_data
 
 
@@ -22,16 +18,12 @@ def project_response_with_duplicate_users():
     home = os.path.dirname(__file__)
     with open(os.path.join(home, '../../fixtures/designsafe_api_project_with_duplicate_users.json'), 'rb') as f:
         mock_data = json.loads(f.read())
-
-    # TODO_TAPISV3 https://tacc-main.atlassian.net/browse/WG-257 can be dropped later as we rely on equests_mock
-    # in tests
-    with patch('geoapi.custom.designsafe.project_users.get_project_data', return_value=mock_data["value"]):
-        yield mock_data["value"]
+        yield mock_data
 
 
 def test_get_system_users(requests_mock, user1, project_response):
     uuid = "5752672753351626260-242ac118-0001-014"
-    requests_mock.get(f"https://agave.designsafe-ci.org/projects/v2/{uuid}/", json=project_response)
+    requests_mock.get(settings.DESIGNSAFE_URL + f"/api/projects/v2/{uuid}/", json=project_response)
 
     users = get_system_users(user1, system_id=f"project-{uuid}")
     users_as_list_of_dict = [{u.username: u.admin} for u in users]
@@ -44,7 +36,7 @@ def test_get_system_users(requests_mock, user1, project_response):
 
 def test_get_system_users_duplicate(requests_mock, user1, project_response_with_duplicate_users):
     uuid = "5752672753351626260-242ac118-0001-014"
-    requests_mock.get(f"https://agave.designsafe-ci.org/projects/v2/{uuid}/", json=project_response_with_duplicate_users)
+    requests_mock.get(settings.DESIGNSAFE_URL + f"/api/projects/v2/{uuid}/", json=project_response_with_duplicate_users)
 
     users = get_system_users(user1, system_id=f"project-{uuid}")
     users_as_list_of_dict = [{u.username: u.admin} for u in users]
