bdd565197739da2a439849e32de45c6d62e846f6
Nathan Franklin <nathan.franklin@gmail.com>
2024-06-25 15:21:33 -0500

    Rework env variables
    * Add TAPIS_CLIENT_ID and  TAPIS_CLIENT_KEY
    * Make env variable usage in docker compose consistent

--- geoapi/custom/designsafe/project.py
+++ geoapi/custom/designsafe/project.py
@@ -10,7 +10,7 @@ def on_project_creation(database_session, user: User, project: Project):
     try:
         logger.debug(f"Creating .hazmapper file for user:{user.username}"
                      f" project:{project.id} project_uuid:{project.uuid} ")
-        deployment = os.getenv("APP_ENV")
+        deployment = settings.APP_ENV
         file_content = json.dumps({"uuid": str(project.uuid), "deployment": deployment})
         file_name = f"{project.system_file}.hazmapper"
         from geoapi.utils.agave import AgaveUtils
@@ -93,7 +93,7 @@ def update_designsafe_project_hazmapper_metadata(user: User, project: Project, a
         new_map_entry = {"name": project.name,
                          "uuid": str(project.uuid),
                          "path": project.system_path,
-                         "deployment": os.getenv("APP_ENV")}
+                         "deployment": settings.APP_ENV}
         all_maps.append(new_map_entry)
     logger.debug(f"Updated metadata for DesignSafe_project:{designsafe_uuid}: {all_maps}")
     response = client.patch(settings.DESIGNSAFE_URL + f"/api/projects/v2/{designsafe_uuid}/",
--- geoapi/settings.py
+++ geoapi/settings.py
@@ -2,14 +2,25 @@ import os
 
 
 class Config(object):
+    # Flask settings
     DEBUG = False
     TESTING = False
-    ASSETS_BASE_DIR = os.environ.get('ASSETS_BASE_DIR', '/assets')
-    JWT_PUB_KEY = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmqQ9Je2o5zydpVY865qao1iX1+E1Kxw3Hpb8FX3PHGjDoOAxrpY2lV0jdXwEPECbYbTv51fu7wBh8p6Y5outup+aRUfAAJxvq0D0w//AQTNE7bYbVNSGlVt68vnywMoFOab2zZ6o83jrlhmvkswb7N48aaZTnY0tCAYJ7SCCj2BN7BsbBoB4Bzv62S86Sw0hMXf3mJlWMoh5bDRANVYbOJ31bzIzJFxCs0hyfLBT8X+d2qSxfF7i33JOIDzOW0GiI0McVJZpifnSbGnLcw/mBGlNB67zwZ9PTsi2TS3s8z0TwnmP9ORM/cpqEfu4yFPvWeADsVAE9h9ZZX6nRCQ91QIDAQAB'  # noqa: E501
+    SECRET_KEY = os.environ.get("FLASK_SESSION_SECRET_KEY")
+
+    # Flask-RESTPlus settings
     RESTPLUS_MASK_SWAGGER = False
+
+    # Tapis-related settings
+    JWT_PUB_KEY = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmqQ9Je2o5zydpVY865qao1iX1+E1Kxw3Hpb8FX3PHGjDoOAxrpY2lV0jdXwEPECbYbTv51fu7wBh8p6Y5outup+aRUfAAJxvq0D0w//AQTNE7bYbVNSGlVt68vnywMoFOab2zZ6o83jrlhmvkswb7N48aaZTnY0tCAYJ7SCCj2BN7BsbBoB4Bzv62S86Sw0hMXf3mJlWMoh5bDRANVYbOJ31bzIzJFxCs0hyfLBT8X+d2qSxfF7i33JOIDzOW0GiI0McVJZpifnSbGnLcw/mBGlNB67zwZ9PTsi2TS3s8z0TwnmP9ORM/cpqEfu4yFPvWeADsVAE9h9ZZX6nRCQ91QIDAQAB'  # noqa: E501
+    TAPIS_CLIENT_ID = os.environ.get("TAPIS_CLIENT_ID")
+    TAPIS_CLIENT_KEY = os.environ.get("TAPIS_CLIENT_KEY")
+
+    # Other settings
+    ASSETS_BASE_DIR = os.environ.get('ASSETS_BASE_DIR', '/assets')
     TENANT = os.environ.get("TENANT")
     STREETVIEW_DIR = os.environ.get('STREETVIEW_DIR', '/assets/streetview')
     DESIGNSAFE_URL = os.environ.get("DESIGNSAFE_URL")
+    APP_ENV = os.environ.get("APP_ENV")
 
 
 class DeployedConfig(Config):
@@ -46,6 +57,9 @@ class UnitTestingConfig(LocalDevelopmentConfig):
     STREETVIEW_DIR = os.environ.get('STREETVIEW_DIR', '/tmp/streetview')
     ASSETS_BASE_DIR = '/tmp'
     DESIGNSAFE_URL = os.environ.get("DESIGNSAFE_URL", "https://designsafe-not-real.tacc.utexas.edu")
+    TAPIS_CLIENT_ID = os.environ.get("TAPIS_CLIENT_ID", "client_id")
+    TAPIS_CLIENT_KEY = os.environ.get("TAPIS_CLIENT_KEY", "client_key_1234")
+    SECRET_KEY = os.environ.get("FLASK_SESSION_SECRET_KEY", "flask_session_secret_key_1234")
 
 
 APP_ENV = os.environ.get('APP_ENV', '').lower()
@@ -53,5 +67,7 @@ if APP_ENV == 'production' or APP_ENV == 'staging' or APP_ENV == 'dev' or APP_EN
     settings = DeployedConfig
 elif APP_ENV == 'testing':
     settings = UnitTestingConfig
-else:
+elif APP_ENV == 'local':
     settings = LocalDevelopmentConfig
+else:
+    raise Exception("APP_ENV is not defined")
