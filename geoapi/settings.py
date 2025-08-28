f68ce2b2816ed654305ba92df7d7a84763749f33
Sal Tijerina <r.sal.tijerina@gmail.com>
2025-08-28 12:04:52 -0500

    remove references to flask

--- geoapi/settings.py
+++ geoapi/settings.py
@@ -2,13 +2,10 @@ import os
 
 
 class Config(object):
-    # Flask settings
+    # Litestar settings
     DEBUG = False
     TESTING = False
-    SECRET_KEY = os.environ.get("FLASK_SESSION_SECRET_KEY")
-
-    # Flask-RESTPlus settings
-    RESTPLUS_MASK_SWAGGER = False
+    SECRET_KEY = os.environ.get("SESSION_SECRET_KEY")
 
     # Tapis-related settings
     TAPIS_PUB_KEY_FOR_VALIDATION = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAmqQ9Je2o5zydpVY865qao1iX1+E1Kxw3Hpb8FX3PHGjDoOAxrpY2lV0jdXwEPECbYbTv51fu7wBh8p6Y5outup+aRUfAAJxvq0D0w//AQTNE7bYbVNSGlVt68vnywMoFOab2zZ6o83jrlhmvkswb7N48aaZTnY0tCAYJ7SCCj2BN7BsbBoB4Bzv62S86Sw0hMXf3mJlWMoh5bDRANVYbOJ31bzIzJFxCs0hyfLBT8X+d2qSxfF7i33JOIDzOW0GiI0McVJZpifnSbGnLcw/mBGlNB67zwZ9PTsi2TS3s8z0TwnmP9ORM/cpqEfu4yFPvWeADsVAE9h9ZZX6nRCQ91QIDAQAB"  # noqa: E501
@@ -78,9 +75,7 @@ class UnitTestingConfig(LocalDevelopmentConfig):
     )
     TAPIS_CLIENT_ID = "test_client_id"
     TAPIS_CLIENT_KEY = "test_client_key_1234"
-    SECRET_KEY = os.environ.get(
-        "FLASK_SESSION_SECRET_KEY", "flask_session_secret_key_1234"
-    )
+    SECRET_KEY = os.environ.get("SESSION_SECRET_KEY", "session_secret_key_1234")
 
 
 APP_ENV = os.environ.get("APP_ENV", "").lower()
