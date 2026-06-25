03b1a479442c3b98d33c0a5c36dc5d20f5f9f7a9
Nathan Franklin <nathan.franklin@gmail.com>
2026-06-24 19:16:48 -0500

    Drop unused import

--- geoapi/services/users.py
+++ geoapi/services/users.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta, timezone
 import requests
 from sqlalchemy.exc import InvalidRequestError
-from redis.exceptions import RedisError
 
 from geoapi.models import Auth, User, Project, ProjectUser
 from geoapi.utils import jwt_utils
